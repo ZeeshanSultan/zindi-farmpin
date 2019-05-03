ab1e5c28a84fdca5b12aa2a9505a81250df450ad
Renier Botha <r.botha91@gmail.com>
2019-05-03 18:35:10 +0200

    reinitialize with better cookie cutter template for colaboration

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
-from setuptools import find_packages, setup
+import os
+
+from setuptools import setup, find_packages
+
+
+def readme():
+    """
+    Utility function to read the README file.
+    Used for the long_description.  It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+    :return: String
+    """
+    return open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
 
 setup(
-    name='src',
-    packages=find_packages(),
+    name='zindi_farm_pin',
     version='0.1.0',
-    description='Farm pin competition on Zindi',
+    url='',
+    license='',
     author='Renier Botha',
-    license='MIT',
+    author_email='r.botha91@gmail.com',
+    description='Solution to the crop classification challenge on Zindi',
+    python_requires='>=3',
+    long_description=readme(),
 )
