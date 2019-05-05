b60d4bfe35208eb76a32452ee77ba3fc9e3f5c77
Renier Botha <r.botha91@gmail.com>
2019-05-05 16:37:03 +0200

    Cleans invoke tasks

--- tasks.py
+++ tasks.py
@@ -1,15 +1,6 @@
 from invoke import task
 
 
-@task
-def create_baseline_dataset(ctx):
-    from src.data.make_baseline_dataset import run
-    print('Creating training set\n')
-    run('train')
-    print('\nCreating testing set\n')
-    run('test')
-    print('done!')
-
 @task(help={
     'ip': 'IP to listen on, defaults to *',
     'extra': 'Port to listen on, defaults to 8888',
@@ -34,21 +25,8 @@ def notebook(ctx, ip='*', port=8888):
     ctx.run(' '.join(cmd))
 
 
-@task(help={
-    'search': 'search term'
-})
-def competition_list(ctx, search=None):
-    """
-    List Kaggle competitions
-    """
-    cmd = ['kaggle competitions list']
-    if search:
-        cmd = cmd + ['-s {}'.format(search)]
-    ctx.run(' '.join(cmd))
-
-
-@task()
-def competition_download_files(ctx):
+@task
+def download_satellite_data(ctx):
     """
     Download Kaggle competition files to ./data/raw folder
     """
@@ -57,14 +35,11 @@ def competition_download_files(ctx):
     ctx.run(cmd)
 
 
-@task(help={
-    'path': 'path to file to submit',
-    'message': 'message describing the submission',
-    'competition': 'competition url prefix'
-})
-def competition_submit_files(ctx, path, message, competition):
-    """
-    Submit Kaggle competition files
-    """
-    cmd = 'kaggle competitions submit', '-c {} -f {} -m {}'.format(competition, path, message)
-    ctx.run(cmd)
+@task
+def create_baseline_dataset(ctx):
+    from src.data.make_baseline_dataset import run
+    print('Creating training set\n')
+    run('train')
+    print('\nCreating testing set\n')
+    run('test')
+    print('done!')
