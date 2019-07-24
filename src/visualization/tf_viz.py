7ebf010b8d2607843e4a5c6b8159d48fdc5cd62a
Renier Botha <r.botha91@gmail.com>
2019-07-24 11:18:51 +0200

    Updates plot losses callback

--- src/visualization/tf_viz.py
+++ src/visualization/tf_viz.py
@@ -2,6 +2,7 @@ from tensorflow import keras
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
 
+
 class PlotLosses(keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
         self.i = 0
@@ -12,15 +13,22 @@ class PlotLosses(keras.callbacks.Callback):
         self.logs = []
 
     def on_epoch_end(self, epoch, logs={}):
-        
+
         self.logs.append(logs)
         self.x.append(self.i)
-        self.losses.append(logs.get('loss'))
-        self.val_losses.append(logs.get('val_loss'))
+        self.losses.append(logs.get("loss"))
+
         self.i += 1
-        
+
         clear_output(wait=True)
-        plt.plot(self.x, self.losses, label="loss ({:.3f})".format(self.losses[-1]))
-        plt.plot(self.x, self.val_losses, label="val_loss ({:.3f})".format(self.val_losses[-1]))
+        plt.plot(self.x, self.losses, label="loss ({:.3f})".format(min(self.losses)))
+        if "val_loss" in logs:
+            self.val_losses.append(logs.get("val_loss"))
+            plt.plot(
+                self.x,
+                self.val_losses,
+                label="val_loss ({:.3f})".format(min(self.val_losses)),
+            )
         plt.legend()
-        plt.show();
+        plt.show()
+
